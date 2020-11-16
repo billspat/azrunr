dbe77959577ad8126a9b0001317cdaa489ebddbd
Patrick Bills <billspat@msu.edu>
2020-11-16 10:46:44 -0500

    check that azure stuff is setup before testing

--- tests/testthat/test_clone.R
+++ tests/testthat/test_clone.R
@@ -9,7 +9,7 @@ test_that("can make vm of zipkin lab image", {
     image_to_clone = "/subscriptions/aa32e19c-49b5-478c-af56-fc710b1a8a1c/resourceGroups/zipkinlab/providers/Microsoft.Compute/images/will3298OSImage"
     #TODO test that this is a valid disk image, or a new fn that, given an image name, checks if it's valid
     # get the current rresource group
-    rg <- get_rg() # logs in and sets up options if not logged in
+    resourceGroup <- get_rg() # logs in and sets up options if not logged in
 
     # set some default values for test
     new_vm_name = paste0("test-", format(Sys.time(), "%Y%m%d-%H%M%S"))
@@ -18,7 +18,13 @@ test_that("can make vm of zipkin lab image", {
 
     # create vm from disk image
     # this takes a while as the test waits for imge to be deployed
-    test_vm<- azrunr::vmFromImage(new_vm_name, rg, image_to_clone, vmUsername=admin_user_name, vmPassword=admin_user_pw)
+    test_vm<- azrunr::vmFromImage(newVMName=new_vm_name,
+                                  resourceGroup=resourceGroup,
+                                  imageID=image_to_clone,
+                                  vmUsername=admin_user_name,
+                                  vmPassword=admin_user_pw,
+                                  comment="created by azrunr test"
+                                  )
 
     testthat::expect_equal(class(test_vm)[1], "az_vm_template")
     testthat::expect_equal(test_vm$name, new_vm_name)
